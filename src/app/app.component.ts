4d47d48bcc1c3bfcaa9b2c3f1bfd5e695e04a8f3
Flavio Tosta <ftosta@debian.mshome.net>
2021-07-22 21:54:44 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'custom-themes';
+export class AppComponent implements OnInit {
+  
+  title = 'custom-themes';  
+  themeLoaded = false;
+  
+  doc: Document;
+
+  constructor(@Inject(DOCUMENT) _doc) {
+    this.doc = _doc;
+  }
+
+  ngOnInit(): void {
+    this.getThemeFromCustomer()
+    .pipe(delay(3000))
+    .subscribe(theme => {
+      let root = this.doc.documentElement;
+      const secondaryColor = this.lightenDarkenColor(theme.primary, -25);
+      theme.secondary = secondaryColor;
+      
+      root.style.setProperty('--primary', theme.primary);
+      root.style.setProperty('--secondary', theme.secondary);
+
+      this.themeLoaded = true;
+    });
+  }
+
+  /**
+   * Custom logic that uses URL and fetches the BE for the theme
+   * @returns theme
+   */
+  private getThemeFromCustomer(): Observable<any> {
+    return of({
+      // azul
+      primary: '#0d6efd',
+      // verde
+      // primary: '#157347'
+    });
+  }
+
+  private lightenDarkenColor(col, amt) {
+  
+    var usePound = false;
+  
+    if (col[0] == "#") {
+        col = col.slice(1);
+        usePound = true;
+    }
+ 
+    var num = parseInt(col,16);
+ 
+    var r = (num >> 16) + amt;
+ 
+    if (r > 255) r = 255;
+    else if  (r < 0) r = 0;
+ 
+    var b = ((num >> 8) & 0x00FF) + amt;
+ 
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+ 
+    var g = (num & 0x0000FF) + amt;
+ 
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+ 
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule
   ],
